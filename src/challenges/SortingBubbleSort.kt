2d0033afe18c0630745cdf1df760af3344e1b070
MaximoDouglas <douglas.h.maximo@gmail.com>
2025-02-17 16:42:27 -0300

    Add SortingBubbleSort challenge solution

--- src/challenges/SortingBubbleSort.kt
+++ src/challenges/SortingBubbleSort.kt
@@ -3,7 +3,29 @@ package challenges.challenges
 // https://www.hackerrank.com/challenges/ctci-bubble-sort/problem?isFullScreen=true&h_l=interview&
 // playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=sorting
 
-fun countSwaps(a: Array<Int>): Unit {
-    // Write your code here
+fun Array<Int>.countSwaps(): CountSwapsResult {
+    var swapsMade = 0
 
+    for (i in indices) {
+        for (j in size.dec() downTo i.inc()) {
+            if (get(j) < get(j.dec())) {
+                swapsMade++
+                val aux = this[j]
+                this[j] = this[j.dec()]
+                this[j.dec()] = aux
+            }
+        }
+    }
+
+    return CountSwapsResult(
+        "Array is sorted in $swapsMade swaps.",
+        "First Element: ${get(0)}",
+        "Last Element: ${get(size.dec())}"
+    )
 }
+
+data class CountSwapsResult(
+    val messageLineOne: String,
+    val messageLineTwo: String,
+    val messageLineThree: String,
+)
--- test/challenges/SortingBubbleSortTest.kt
+++ test/challenges/SortingBubbleSortTest.kt
@@ -1,24 +1,61 @@
 package challenges
 
+import challenges.challenges.CountSwapsResult
+import challenges.challenges.countSwaps
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
+import kotlin.test.assertContentEquals
+import kotlin.test.assertEquals
 
 @RunWith(JUnit4::class)
 class SortingBubbleSortTest {
 
     @Test
     fun `The sum works correctly on entry1`() {
+        val originalArray = arrayOf(6, 4, 1)
+        val expectedArray = arrayOf(1, 4, 6)
+        val expectedResult = CountSwapsResult(
+            messageLineOne = "Array is sorted in 3 swaps.",
+            messageLineTwo = "First Element: 1",
+            messageLineThree = "Last Element: 6"
+        )
 
+        val result = originalArray.countSwaps()
+
+        assertContentEquals(expectedArray, originalArray)
+        assertEquals(expectedResult, result)
     }
 
     @Test
     fun `The sum works correctly on entry2`() {
+        val originalArray = arrayOf(1, 2, 3)
+        val expectedArray = arrayOf(1, 2, 3)
+        val expectedResult = CountSwapsResult(
+            messageLineOne = "Array is sorted in 0 swaps.",
+            messageLineTwo = "First Element: 1",
+            messageLineThree = "Last Element: 3"
+        )
 
+        val result = originalArray.countSwaps()
+
+        assertContentEquals(expectedArray, originalArray)
+        assertEquals(expectedResult, result)
     }
 
     @Test
     fun `The sum works correctly on entry3`() {
+        val originalArray = arrayOf(3, 2, 1)
+        val expectedArray = arrayOf(1, 2, 3)
+        val expectedResult = CountSwapsResult(
+            messageLineOne = "Array is sorted in 3 swaps.",
+            messageLineTwo = "First Element: 1",
+            messageLineThree = "Last Element: 3"
+        )
 
+        val result = originalArray.countSwaps()
+
+        assertContentEquals(expectedArray, originalArray)
+        assertEquals(expectedResult, result)
     }
 }
