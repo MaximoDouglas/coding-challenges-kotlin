d14965e161a191eec93893c8fd3c6df4aff012ae
MaximoDouglas <douglas.h.maximo@gmail.com>
2025-03-19 23:39:05 -0300

    Simplify luck-balance

--- src/challenges/LuckBalance.kt
+++ src/challenges/LuckBalance.kt
@@ -2,8 +2,9 @@ package challenges.challenges
 
 // https://www.hackerrank.com/challenges/luck-balance
 
+
 fun luckBalance(k: Int, contests: Array<Array<Int>>): Int {
-    val orderedContests = mergeSort(contests)
+    val orderedContests = contests.sortedByDescending { it[0] }
 
     var loseAmount = k
     var luck = 0
@@ -22,44 +23,4 @@ fun luckBalance(k: Int, contests: Array<Array<Int>>): Int {
     }
 
     return luck
-}
-
-private fun mergeSort(array: Array<Array<Int>>, p: Int = 0, r: Int = array.size.dec()): Array<Array<Int>> {
-    if (p >= r) {
-        return array
-    }
-
-    val q = (r.plus(p).floorDiv(2))
-
-    mergeSort(array, p, q)
-    mergeSort(array, q+1, r)
-
-    merge(array, p, q, r)
-
-    return array
-}
-
-private fun merge(array: Array<Array<Int>>, p: Int, q: Int, r: Int) {
-    var i = 0
-    var j = 0
-    var k = p
-
-    val arrayLeft = array.sliceArray(p..q)
-    val arrayRight = array.sliceArray(q.inc()..r)
-
-    while (i < arrayLeft.size) {
-        while (j < arrayRight.size && arrayRight[j][0] >= arrayLeft[i][0]) {
-            array[k] = arrayRight[j]
-            k++
-            j++
-        }
-
-        array[k] = arrayLeft[i]
-        k++
-        i++
-
-        if (i == arrayLeft.size && j < arrayRight.size) {
-            arrayRight.copyInto(array, destinationOffset = k, startIndex = j)
-        }
-    }
 }
