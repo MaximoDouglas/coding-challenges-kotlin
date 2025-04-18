b478d4d38eabf6a6c83d79c2b4840d0b961016cb
Douglas Maximo <dougmaximo@MacBook-Pro-de-Douglas.local>
2025-04-18 01:19:35 -0300

    Add sherlock-and-anagrams

--- src/challenges/SherlockAndAnagrams.kt
+++ src/challenges/SherlockAndAnagrams.kt
@@ -1,6 +1,24 @@
 package challenges.challenges
 
+// https://www.hackerrank.com/challenges/sherlock-and-anagrams
+
 fun sherlockAndAnagrams(s: String): Int {
-    // Write your code here
-    return 0
+    val combinations = mutableMapOf<String, Int>()
+
+    for (i in s.indices) {
+        for (j in i.inc()..s.length) {
+            val key = s.substring(i, j).toCharArray().sorted().joinToString("")
+            val amount = combinations[key] ?: 0
+
+            combinations[key] = amount + 1
+        }
+    }
+
+    var combinationsCounter = 0
+
+    combinations.forEach { (_, value) ->
+        combinationsCounter += (value * (value - 1))/2
+    }
+
+    return combinationsCounter
 }
